d88fbad2ddf849942c1bb688b4b1819d2d954d1a
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-10-19 07:08:00 +0530

    uniquesubsequences of a string using hash set in java

---
+++ uniqueSubsequence.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+public class uniqueSubsequence{
+public static void subsequence(String str,int idx,String newString,HashSet<String> set){
+        if(idx==str.length()){
+            if(set.contains(newString)){
+                return;
+            }
+            else{
+            System.out.println(newString);
+            set.add(newString);
+            return;
+            }
+        }
+        char currChar = str.charAt(idx);
+
+        // choose to be
+        subsequence(str,idx+1,newString+currChar,set);
+
+        // choose not to be
+        subsequence(str,idx+1,newString,set);
+    }
+    public static void main(String []args){
+        String str="aac";
+        HashSet<String> set = new HashSet<>();
+        subsequence(str,0,"",set);
+    }
+}
