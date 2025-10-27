c656254f5dfb0298f1930bd404a98f9ef257c2fb
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-10-27 18:33:56 +0530

    updated with comments

--- keyboardComb.java
+++ keyboardComb.java
@@ -1,16 +1,16 @@
 public class keyboardComb{
     public static String[] keypad ={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
 
-    public static void printComb(String str,int idx,String combination){
+    public static void printComb(String str,int idx,String combination){//("23",0,"");
         if(idx==str.length()){
             System.out.println(combination);
             return;
         }
 
-        char currChar = str.charAt(idx);
-        String mapping = keypad[currChar - '0'];
+        char currChar = str.charAt(idx);//currchar = 2;
+        String mapping = keypad[currChar - '0'];// mapping = keypad['2'-'0'] = keypad[2] = "def";
         for (int i=0;i<mapping.length();i++){
-            printComb(str,idx+1,combination+mapping.charAt(i));
+            printComb(str,idx+1,combination+mapping.charAt(i));//("23",1,"d");("23",1,"de");("23",2,"def");
         }
     }
 
