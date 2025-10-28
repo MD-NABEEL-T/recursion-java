5b34a8b2c59642b01ab7450b63afdbeeccedb1ba
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-10-28 17:52:00 +0530

    updated

--- permutationsString2.java
+++ permutationsString2.java
@@ -1,5 +1,17 @@
 public class PermutationsString2{
+    public static void Permutation(String str,String permutes){
+        if(str.length()==0){
+            System.out.println(permutes);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char currChar=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            Permutation(newStr,permutes+currChar);
+        }
+    }
     public static void main(String[] args){
-        
+        String str="abc";
+        Permutation(str,"");
     }
 }
