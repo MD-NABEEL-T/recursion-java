2ae0762ea0e19dfd626fc497ccb0001b124f15a5
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-11-01 22:59:38 +0530

    update

--- pathsinnm.java
+++ pathsinnm.java
@@ -1,5 +1,7 @@
+// count total paths in a maze to move from (0,0) to (n,m).
+// either we can go right or down 
 public class pathsinm{
     public static void main(String[] args){
-        
+
     }
 }
