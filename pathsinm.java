9764143dbb1b82fbc7533bf0e6339048714da14c
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-11-02 17:52:37 +0530

    updated

--- pathsinm.java
+++ pathsinm.java
@@ -1,20 +1,25 @@
-public class pathsinm{
-    public static int countPaths(int i,int j,int n,int m){
-        if(i==n||j==n){
+public class pathsinm {
+    public static int countPaths(int i, int j, int n, int m) {
+
+        // Out of grid
+        if (i == n || j == m) {
             return 0;
         }
-        if(i==n-1&& j==n-1){
+
+        // Reached end
+        if (i == n - 1 && j == m - 1) {
             return 1;
         }
-        int downPaths=countPaths(i+1,j,n,m);
-        
-        int rightPaths = countPaths(i,j+1,n,m);
-        return downPaths = rightPaths;
-    }
-    public static void main(String[] args){
-        int n=3,m=3;
 
-        int totalPaths = countPaths(0,0,n,m);
+        int downPaths = countPaths(i + 1, j, n, m);   // Move Down
+        int rightPaths = countPaths(i, j + 1, n, m);  // Move Right
+
+        return downPaths + rightPaths;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        int totalPaths = countPaths(0, 0, n, m);
         System.out.println(totalPaths);
     }
 }
